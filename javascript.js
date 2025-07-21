742f5beab0474912fac0c6cc850f3ac377eb9e78
dev909-boop <215330302+dev909-boop@users.noreply.github.com>
2025-07-20 20:17:27 -0400

    Add the intro exercies to Github - pending assignment

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let firstName = "John";
+let surname = "Doe";
+
+console.log(firstName);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+const pi = 3.14;
+//pi = 10;
+
+console.log(pi); // What will be output?
+
+console.log((3 + 2) - 76 * (1 + 1));
